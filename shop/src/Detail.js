28a4a70c5897ed24ed27512ad225b305b32d4707
SloaneY <tpdms717@gmail.com>
2021-06-07 15:30:51 +0900

    ⚡️ Add Sass

--- shop/src/Detail.js
+++ shop/src/Detail.js
@@ -1,8 +1,9 @@
 import React, { useState } from "react";
 import { useHistory, useParams } from "react-router-dom";
+import "./Detail.scss";
 
 const Detail = (props) => {
-  let { id } = useParams();
+  let { id } = useParams(); // {} 중괄호가 생성됨!
   let history = useHistory();
   let 찾은상품 = props.shoes.find(function (상품) {
     return 상품.id == id;
@@ -10,6 +11,9 @@ const Detail = (props) => {
 
   return (
     <div className="container">
+      <div className="my-alert">
+        <p>재고가 얼마 남지 않았습니다.</p>
+      </div>
       <div className="row">
         <div className="col-md-6">
           <img
@@ -22,7 +26,7 @@ const Detail = (props) => {
           />
         </div>
         <div className="col-md-6 mt-4">
-          <h4 className="pt-5">{찾은상품.title}</h4>
+          <h4 className="red pt-5">{찾은상품.title}</h4>
           <p>{찾은상품.content}</p>
           <p>{찾은상품.price}원</p>
           <button className="btn btn-danger">주문하기</button>
